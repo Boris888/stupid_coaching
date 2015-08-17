bf3cdd78522b723186586671078b8c1d3fb01b86
Boris Hruruguen <boris.huruguen@gmail.com>
2015-08-17 12:01:57 +0200

    done

---
+++ app/controllers/playing_controller.rb
@@ -0,0 +1,20 @@
+class PlayingController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+
+ def coach_answer(your_message)
+    if your_message == "I am going to work right now!" || your_message == "I am going to work right now!".upcase
+      return "Good boooooy !"
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'playing/ask'
+
+  get 'playing/answer'
+
+  get '/ask' => 'playing#ask'
+
+  get '/answer' => 'playing#answer'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
